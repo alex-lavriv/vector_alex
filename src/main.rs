4179f4c7ecf57aa7ae6923c7a7753af41e9bc0e8
phantom-il-alex <alex@phantomauto.com>
2023-06-15 12:16:30 +0300

    push

--- src/main.rs
+++ src/main.rs
@@ -15,25 +15,47 @@ fn main() {
     // vector.shrink_to_fit();
     // println!("len: {}, capacity {}", vector.len(), vector.capacity());
 
-    let mut my_vector = vector_alex::new();
+    let mut my_vector = VectorAlex::new();
     assert_eq!(my_vector.len(), 0);
     assert_eq!(my_vector.capacity(), 0);
     my_vector.push(5);
+    assert_eq!(my_vector.get_item(0), 5);
     assert_eq!(my_vector.len(), 1);
     assert_eq!(my_vector.capacity(), 4);
 
+    let upper_lim = 10;
+    for i in 1..upper_lim {
+        println!("Index is {}", i);
+        my_vector.push(i);
+        assert_eq!(my_vector.get_item(i), i);
+        assert_eq!(my_vector.len(), i + 1);
+    }
+    assert_eq!(my_vector.capacity(), 16);
 
+    for i in 1..upper_lim {
+        println!("Index is {}", i);
+        let value =  my_vector.get_item(i);
+        println!("value of index {} is {}", i, value);
+    }
+    for i in 1..upper_lim {
+        println!("Index is {}", i);
+        assert_eq!(my_vector.get_item(i), i);
+
+    }
 }
+
 use std::alloc::{alloc, dealloc, handle_alloc_error, Layout};
 
-struct vector_alex<T>{
+struct VectorAlex<T> {
     len: usize,
-    items: Option<Box<[T]>>
+    cap: usize,
+    items_1: Option<*mut u8>,
+    phantom: std::marker::PhantomData<T>,
 }
 
-impl <T> vector_alex <T> {
-    pub fn new() -> vector_alex<T> {
-        vector_alex{len: 0, items: None}
+impl<T: Default + Copy + Clone> VectorAlex<T> {
+    pub fn new() -> VectorAlex<T> {
+        VectorAlex { len: 0, cap: 0, items_1: None, phantom: std::marker::PhantomData }
     }
     pub fn len(&self) -> usize
     {
@@ -41,39 +63,70 @@ impl <T> vector_alex <T> {
     }
     pub fn capacity(&self) -> usize
     {
-        if let Some(items) = &self.items{
-            items.len()
-        }else{
-            0
-        }
+        self.cap
     }
     pub fn push(&mut self, item: T)
     {
-
-        if self.len() == self.capacity(){
+        if self.len() == self.capacity() {
             self.reallloc();
         }
 
-        if let Some(items) = &mut self.items{
-            items[self.len] = item;
+        self.set_item(item, self.len);
+        self.len += 1;
+    }
+    fn set_item(&mut self, item: T, index: usize) {
+        if let Some(items) = self.items_1 {
+            Self::set_item_ptr(items, item, index)
         }
     }
-    fn reallloc(&mut self){
+    fn set_item_ptr(ptr: *mut u8, item: T, index: usize) {
         unsafe {
-            let layout = Layout::array(4).unwrap();
-            let ptr = alloc(layout);
-            if ptr.is_null() {
-                handle_alloc_error(layout);
-            }
+            *(ptr.offset((index * 8) as isize) as *mut [T; 1]) = [item]
+        };
+    }
+    pub fn get_item_ptr(ptr: *mut u8, index: usize) -> T {
+        unsafe {
+                return (*(ptr.offset((index * 8) as isize) as *mut [T; 1]))[0].clone();
 
-            *(ptr as *mut u16) = 42;
-            assert_eq!(*(ptr as *mut u16), 42);
-
-            dealloc(ptr, layout);
         }
     }
+    pub fn get_item(&mut self, index: usize) -> T {
+            if let Some(items) = self.items_1 {
+               return Self::get_item_ptr(items, index);
+            }
+            Default::default()
+    }
+    fn reallloc(&mut self) {
+        unsafe {
+            if let Some(prev_items) = self.items_1 {
+                let next_cap = self.cap * 2;
+                let layout = Layout::array::<T>(next_cap).unwrap();
+                self.cap = next_cap;
+                let ptr = alloc(layout);
+                if ptr.is_null() {
+                    handle_alloc_error(layout);
+                }
+                self.cap = next_cap;
+                // copy prev arr
+                for i in 0..self.len {
+                    let prev_val = Self::get_item_ptr(prev_items, i);
+                    Self::set_item_ptr(ptr, prev_val, i);
+                }
+                self.items_1 = Some(ptr);
+            } else {
+                self.cap = 4;
+                let layout = Layout::array::<T>(self.cap).unwrap();
 
-
+                let ptr = alloc(layout);
+                if ptr.is_null() {
+                    handle_alloc_error(layout);
+                }
+                self.items_1 = Some(ptr);
+            }
+        }
+    }
 }
+
+
 
 
